592f3e125843424347bf383d19ae563a2dae1d20
Tom Hicks <hickst@tohono.com>
2021-11-29 19:10:42 -0700

    Remove SQL scripts for deleted TAP, Jaguar, and catalog tables.

--- dropTapSchema.sql
+++
@@ -1,7 +0,0 @@
---
--- Drop the tap_schema schema from the VOS database.
---
-REVOKE USAGE ON SCHEMA tap_schema FROM tapuser;
-REVOKE USAGE ON SCHEMA tap_schema FROM alquery;
-REVOKE USAGE ON SCHEMA tap_schema FROM astrolabe;
-DROP SCHEMA tap_schema CASCADE;
